c6f90ad00069e26abcea9102bdea49ae1de84575
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-02-01 20:53:14 +1100

    Trusted by top companies banner completed (home page)

---
+++ src/components/home/topCompanies/TopCompanies.jsx
@@ -0,0 +1,71 @@
+import styles from "./topCompanies.module.css";
+import johnHolland from "../../../assets/home/topCompanies/john_holland.png";
+import lendlease from "../../../assets/home/topCompanies/lendlease.svg";
+import richardCrockes from "../../../assets/home/topCompanies/richard_crockes.png";
+import cpb from "../../../assets/home/topCompanies/cpb_contractors.png";
+import NSWGov from "../../../assets/home/topCompanies/NSW_Gov.svg";
+const TopCompanies = () => {
+  return (
+    <section className={styles.relativeBox}>
+      <h2 className={styles.title}> Trusted by Top Companies</h2>
+      <div className={styles.topCompaniesSection}>
+        <div className={styles.infiniteScrollBanner}>
+          <img
+            className={styles.bannerImg}
+            src={johnHolland}
+            alt="john holland logo"
+          />
+          <img
+            className={styles.bannerImg}
+            src={lendlease}
+            alt="lendlease logo"
+          />
+          <img
+            className={styles.bannerImg}
+            src={richardCrockes}
+            alt="richard crockes logo"
+          />
+          <img
+            className={styles.bannerImg}
+            src={cpb}
+            alt="cpb contractors logo"
+          />
+          <img
+            className={styles.bannerImg}
+            src={NSWGov}
+            alt="New South Wales Government logo"
+          />
+        </div>
+        <div className={styles.infiniteScrollBanner}>
+          <img
+            className={styles.bannerImg}
+            src={johnHolland}
+            alt="john holland logo"
+          />
+          <img
+            className={styles.bannerImg}
+            src={lendlease}
+            alt="lendlease logo"
+          />
+          <img
+            className={styles.bannerImg}
+            src={richardCrockes}
+            alt="richard crockes logo"
+          />
+          <img
+            className={styles.bannerImg}
+            src={cpb}
+            alt="cpb contractors logo"
+          />
+          <img
+            className={styles.bannerImg}
+            src={NSWGov}
+            alt="New South Wales Government logo"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopCompanies;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Services from "../components/home/services/services";
+import TopCompanies from "../components/home/topCompanies/topCompanies";
 const Home = () => {
   return (
     <div>
       Hello Home
       <Services />
+      <TopCompanies />
     </div>
   );
 };
